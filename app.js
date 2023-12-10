7cb8ed401dff5003008a7a16f12e67889f7789f1
Ku Zaharul Asyraf <kzasyraf@gmail.com>
2023-12-10 22:26:36 +0800

    Update Form

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const deploymentId = "AKfycbyjDacz6vpgDObKexG7TuQeToKBXdxK2CLJCqXvjVIpfIyGdygDA5vnga5BTOuM751L"
+const deploymentId = "AKfycbw7m5wO_6Gq65qyTfAuZxzFn-0V6IsmHjBxQLYx9uk0mM2XuoAtKQ-Gzwb0oZO-ZdcD"
 const apiUrl = "https://script.google.com/macros/s/" + deploymentId + "/exec";
 
 function getData() {
@@ -11,36 +11,62 @@ function getData() {
         },
         redirect: "follow",
     })
-    .then(d => d.ok ? d.json() : '')
-    .then(d => {
-        document.getElementById('app').textContent = d.status;
-    });
+        .then(d => d.ok ? d.json() : '')
+        .then(d => {
+            document.getElementById('app').textContent = d.status;
+        });
 }
 
-async function postData() {
-    const data = {
-        "First Name": "My Name",
-        "Last Name": "is John",
-        "Application": "google",
-        "Date": "2023-12-20T17:39:48.833Z"
-    };
+async function postData(data) {
     let response = await fetch(apiUrl, {
         method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
-          "Accept": "application/json",
-          "Accept-Encoding": "gzip, deflate, br",
-          "Connection": "keep-alive"
+            "Accept": "application/json",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive"
         },
         redirect: "follow", // manual, *follow, error
         body: JSON.stringify(data) // body data type must match "Content-Type" header
     })
-    .then(d => (d.ok || d.redirected) ? d.json() : '')
-    .then(d => {
-        document.getElementById('app').textContent = d.status;
-    });
+        .then(d => (d.ok || d.redirected) ? d.json() : '')
+        .then(d => (d.status === 201) ? true : false);
 }
 
-document.getElementById('doGet').addEventListener('click',getData);
-document.getElementById('doPost').addEventListener('click',postData);
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+        keyboard: false
+    });
+    const myToggle = document.getElementById('doPost');
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            } else {
+                var inputName = document.getElementById('inputName');
+                var inputAttendance = document.getElementById('inputAttendance');
+                var inputWished = document.getElementById('inputWished');
+                postData({
+                    name: inputName.value,
+                    attendance: inputAttendance.value,
+                    wished: inputWished.value
+                });
+                inputName.value = '';
+                inputAttendance.selectedIndex = 0;
+                inputWished.value = '';
+                myModal.show(myToggle);
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
