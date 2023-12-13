33aee2cff36711d2bd276a22a634436fa8920f71
Ku Zaharul Asyraf <kzasyraf@gmail.com>
2023-12-14 01:00:19 +0800

    Update form validation on ios

--- app.js
+++ app.js
@@ -84,4 +84,19 @@ async function postData(table, data) {
             location.reload(true);
         }, false);
     });
+
+    function hasHtml5Validation () {
+		return typeof document.createElement('input').checkValidity === 'function';
+	}
+
+	if (hasHtml5Validation()) {
+		forms.submit(function (e) {
+		if (!this.checkValidity()) {
+			e.preventDefault();
+			$(this).addClass('invalid');
+		} else {
+			$(this).removeClass('invalid');
+		}
+		});
+	}
 })()
