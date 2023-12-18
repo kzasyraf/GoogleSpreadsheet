cd985e64503be628b6be5406fea0586f287650ce
Ku Zaharul Asyraf <kzasyraf@gmail.com>
2023-12-18 18:05:43 +0800

    Update for mobile test

--- app_test/app_fetch.js
+++ app_test/app_fetch.js
@@ -1,15 +1,15 @@
 (async () => {
     'use strict'
-    const deploymentId = "AKfycbxDmRJSC3dyP1SkWdd2Tq5ubi72Z1hy0w2DZJZeX-_OD8W3FRy39nE5q76R7SisFPVb";
-    const apiUrl = "https://script.google.com/macros/s/" + deploymentId + "/exec"
+    const deploymentId = 'AKfycbxDmRJSC3dyP1SkWdd2Tq5ubi72Z1hy0w2DZJZeX-_OD8W3FRy39nE5q76R7SisFPVb';
+    const apiUrl = 'https://script.google.com/macros/s/' + deploymentId + '/exec'
 
     const authInfo = await fetch(apiUrl + '?hub.mode=publish&hub.verify_token=a136e25f-1cb8-4d1c-af73-142c678398d0', {
-        mode: "cors",
-        cache: "no-cache",
+        mode: 'cors',
+        cache: 'no-cache',
         headers: {
-            Accept: "application/json",
+            Accept: 'application/json',
         },
-        redirect: "follow",
+        redirect: 'follow',
     })
     .then(d => d.json())
     .catch(e => console.log(e));
@@ -17,17 +17,18 @@
     console.log('fetch: ' + authInfo);
     
     const post = await fetch(apiUrl + '/data?hub.table=attendance_list', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // change to cors when publish to github
-        cache: "no-cache",
-        credentials: "include",
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // change to cors when publish to github
+        cache: 'no-cache',
+        credentials: 'include',
         headers: {
             Authorization: `${authInfo.token_type} ${authInfo.access_token}`,
-            Accept: "application/json"
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
         },
-        redirect: "follow",
-        referrer: "",
-        referrerPolicy: "origin-when-cross-origin",
+        redirect: 'follow',
+        referrer: '',
+        referrerPolicy: 'origin-when-cross-origin',
         keepalive: true,
         body: JSON.stringify({ name: 'Hello', attendance: 'attending', wished: (new Date()).toISOString() })
     }).then(d => d.type);
